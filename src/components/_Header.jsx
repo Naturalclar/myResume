b8992c15b5c11ecfc848e0fb1693276a56020f38
Jesse Katsumata <jesse.katsumata@gmail.com>
2018-02-08 15:18:57 -0800

    Hide Street Address

--- src/components/_Header.jsx
+++ src/components/_Header.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Header = () => (
   <header>
     <div className="name">Jesse Katsumata</div>
-    <div className="address">4613 Korbel St. Union City, CA 94587</div>
+    <div className="address">Union City, CA 94587</div>
     <div className="contact">Phone: (510) 552-6792 - Email: jesse.katsumata@gmail.com</div>
   </header>
 );
