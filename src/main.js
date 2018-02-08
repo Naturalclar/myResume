050af6b960d94c3d5922057eb0d34c093afed021
Jesse Katsumata <jesse.katsumata@gmail.com>
2018-02-08 14:34:38 -0800

    apply global css with styled-components

--- src/main.js
+++ src/main.js
@@ -1,9 +1,25 @@
+/* eslint-disable */
 import React from 'react';
 import { render } from 'react-dom';
+import { injectGlobal } from 'styled-components';
 import 'normalize.css';
 import 'font-awesome/css/font-awesome.css';
 import './styles.css';
 import App from './components/App';
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Merriweather|Open+Sans');
+  * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: azure;
+    font-family: 'Merriweather', serif;
+    margin: 0;
+    padding: 0;
+  }
+`;
 
 render(
   <App />,
