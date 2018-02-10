ec72c20bfdd7b9b7396977719f898b066fa9fe95
Jesse Katsumata <jesse.katsumata@gmail.com>
2018-02-09 17:38:21 -0800

    Add Preact

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
   },
   resolve: {
     extensions: ['.js', '.jsx'],
+    alias: {
+      react: 'preact-compat',
+      'react-dom': 'preact-compat',
+    },
   },
   module: {
     loaders: [
