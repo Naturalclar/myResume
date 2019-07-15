3867d1dd3472c6d7637835c29fa38808ca46c1b9
Naturalclar <jesse.katsumata@gmail.com>
2019-07-16 00:49:56 +0900

    :wrench: add eslintrc and prettier config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,31 @@
 module.exports = {
-    "extends": "airbnb"
-};
+  extends: [
+    'airbnb',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'react/jsx-filename-extension': [2, { extensions: ['.tsx', '.ts'] }],
+    'import/extensions': [
+      2,
+      { extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'] },
+    ],
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/indent': ['error', 2],
+  },
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.ts', '.tsx'],
+      },
+    },
+  },
+}
